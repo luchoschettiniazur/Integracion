921dc4101b7d5c443093b352d77cedc378d7804b
luchoschettiniazur <luchoschettiniazur@outlook.com>
2024-06-03 22:38:31 +0200

    first commit

---
+++ API/Controllers/CategoriaController.cs
@@ -0,0 +1,33 @@
+﻿using Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriaController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public CategoriaController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+
+    [HttpGet]
+    public async Task<ActionResult<List<Categoria>>> GetCategorias()
+    {
+        List<Categoria> lista = await _context.Categorias.ToListAsync();
+        return Ok(lista);
+    }
+
+}
+
+
+
+
+
---
+++ Models/Entidades/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Entidades;
+
+[Table("Categoria")]
+public class Categoria
+{
+    [Key]
+    [Column("CategoriaId")]
+    public int Id { get; set; }
+
+    public string Nombre { get; set; }
+}
